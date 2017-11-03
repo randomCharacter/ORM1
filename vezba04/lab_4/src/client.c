04488c49fc60487015b234a2fe73b2130c8336ed
Mario Peric <pericmario96@gmail.com>
2017-11-03 01:08:04 +0000

    vezba04

---
+++ vezba04/lab_4/src/client.c
@@ -0,0 +1,70 @@
+/* 
+    ********************************************************************
+    Odsek:          Elektrotehnika i racunarstvo
+    Departman:      Racunarstvo i automatika
+    Katedra:        Racunarska tehnika i racunarske komunikacije (RT-RK)
+    Predmet:        Osnovi Racunarskih Mreza 1
+    Godina studija: Treca (III)
+    Skolska godina: 2017/2018
+    Semestar:       Zimski (V)
+    
+    Ime fajla:      client.c
+    Opis:           TCP/IP klijent
+    
+    Platforma:      Raspberry Pi 2 - Model B
+    OS:             Raspbian
+    ********************************************************************
+*/
+
+#include<stdio.h>      //printf
+#include<string.h>     //strlen
+#include<sys/socket.h> //socket
+#include<arpa/inet.h>  //inet_addr
+#include <fcntl.h>     //for open
+#include <unistd.h>    //for close
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT   27015
+
+int main(int argc , char *argv[])
+{
+    int sock;
+    struct sockaddr_in server;
+    char *message = "this is a test";
+
+    //Create socket
+    sock = socket(AF_INET , SOCK_STREAM , 0);
+    if (sock == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons(DEFAULT_PORT);
+
+    //Connect to remote server
+    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        perror("connect failed. Error");
+        return 1;
+    }
+
+    puts("Connected\n");
+
+    //Send some data
+    if( send(sock , message , strlen(message), 0) < 0)
+    {
+        puts("Send failed");
+        return 1;
+    }
+
+    puts("Client message:");
+    puts(message);
+
+    close(sock);
+
+    return 0;
+}
+
---
+++ vezba04/lab_4/src/server.c
@@ -0,0 +1,90 @@
+/* 
+    ********************************************************************
+    Odsek:          Elektrotehnika i racunarstvo
+    Departman:      Racunarstvo i automatika
+    Katedra:        Racunarska tehnika i racunarske komunikacije (RT-RK)
+    Predmet:        Osnovi Racunarskih Mreza 1
+    Godina studija: Treca (III)
+    Skolska godina: 2017/2018
+    Semestar:       Zimski (V)
+    
+    Ime fajla:      server.c
+    Opis:           TCP/IP server
+    
+    Platforma:      Raspberry Pi 2 - Model B
+    OS:             Raspbian
+    ********************************************************************
+*/
+
+#include<stdio.h>
+#include<string.h>    //strlen
+#include<sys/socket.h>
+#include<arpa/inet.h> //inet_addr
+#include<unistd.h>    //write
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT   27015
+
+int main(int argc , char *argv[])
+{
+    int socket_desc , client_sock , c , read_size;
+    struct sockaddr_in server , client;
+    char client_message[DEFAULT_BUFLEN];
+   
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(DEFAULT_PORT);
+
+    //Bind
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        //print the error message
+        perror("bind failed. Error");
+        return 1;
+    }
+    puts("bind done");
+
+    //Listen
+    listen(socket_desc , 3);
+
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+
+    //accept connection from an incoming client
+    client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+    if (client_sock < 0)
+    {
+        perror("accept failed");
+        return 1;
+    }
+    puts("Connection accepted");
+
+    //Receive a message from client
+    while( (read_size = recv(client_sock , client_message , DEFAULT_BUFLEN , 0)) > 0 )
+    {
+        printf("Bytes received: %d\n", read_size);
+    }
+
+    if(read_size == 0)
+    {
+        puts("Client disconnected");
+        fflush(stdout);
+    }
+    else if(read_size == -1)
+    {
+        perror("recv failed");
+    }
+
+    return 0;
+}
+
